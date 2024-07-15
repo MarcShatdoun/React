31fe18c75565bd981956fd63434fb2e4984a27ef
MarcQuimis <marc.quimis@gmail.com>
2024-07-15 22:09:40 +0200

    boton reactivo

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,5 +1,16 @@
-export function TwitterFollowCard ({formatUserName, userName, name, isFollowing}) {
+import { useState } from 'react' 
+export function TwitterFollowCard ({formatUserName, userName, name,}) {
+  const [isFollowing, setIsFollowing] = useState(false)
+    const text = isFollowing ? 'Siguiendo': 'Seguir';
     const imgSrc = `https://unavatar.io/${userName}`
+    const buttonClassName = isFollowing ? 
+    'tw-followCard-button is-following' 
+    : 'tw-followCard-button '
+
+    let handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+    
     
     return (
         <article className="tw-followCard">
@@ -9,14 +20,14 @@ export function TwitterFollowCard ({formatUserName, userName, name, isFollowing}
            src={imgSrc} 
            alt={`Foto de ${userName}`} />
   
-          <div className='tw-followCard-info'>
+          <div className='tw-followCard-info'>  
             <strong>{name}</strong>
             <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
           </div>
         </header>
         <aside className='tw-followCard-user'>
-          <button className='tw-followCard-button'>
-            Seguir
+          <button className={buttonClassName} onClick={handleClick}>
+            {text}
           </button>
         </aside>
       </article>
